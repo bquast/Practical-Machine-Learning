43ec829790ddd52296a80a3d422f84cec46744cd
Bastiaan Quast <bquast@gmail.com>
2014-07-21 12:51:52 +0200

    update quiz 2

--- quiz2.R
+++ quiz2.R
@@ -8,7 +8,6 @@ data(AlzheimerDisease)
 
 adData = data.frame(predictors)
 trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
-trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
 training = adData[trainIndex,]
 testing = adData[-trainIndex,]
 
@@ -17,31 +16,55 @@ library(AppliedPredictiveModeling)
 data(concrete)
 library(caret)
 set.seed(975)
-inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+inTrain = createDataPartition(mixtures$CompressiveStrength,
+                              p = 3/4)[[1]]
 training = mixtures[ inTrain,]
 testing = mixtures[-inTrain,]
-str(training)
-?hist
-?qplot
-qplot(CompressiveStrength, data=training)
+featurePlot(x = training[,c('Cement',
+                            'BlastFurnaceSlag',
+                            'FlyAsh',
+                            'Water',
+                            'Superplasticizer',
+                            'CoarseAggregate',
+                            'FineAggregate', 'Age')],
+            y = training$CompressiveStrength )
 
 # question 3
 library(AppliedPredictiveModeling)
 data(concrete)
 library(caret)
 set.seed(975)
-inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+inTrain = createDataPartition(mixtures$CompressiveStrength,
+                              p = 3/4)[[1]]
 training = mixtures[ inTrain,]
 testing = mixtures[-inTrain,]
 qplot(Superplasticizer, data=training)
-names(training)[5]
+
 
 # question 4
-set.seed(3433)
 library(AppliedPredictiveModeling)
-data(AlzheimerDisease)
 library(caret)
+data(AlzheimerDisease)
+set.seed(3433)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis,
+                              p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+ss <- training[,grep('^IL', x = names(training) )]
+preProc <- preProcess(ss, method='pca', thresh = 0.8, outcome = training$diagnosis)
+
+
+# question 5
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
 adData = data.frame(diagnosis,predictors)
 inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
 training = adData[ inTrain,]
 testing = adData[-inTrain,]
+ss <- testing[,grep('^IL', x = names(testing) )]
+model1 <- train(ss, testing$diagnosis, method='glm')
+model2 <- preProcess(ss, method='pca', thresh = 0.8, outcome = testing$diagnosis)
